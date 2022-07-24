18ebb57f4993c56ac9d08aeb1d9742dfc66ad66e
RomaSamkov <Romansamkov.rs@gmail.com>
2022-07-24 22:07:39 +0300

    add src folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,22 +6,35 @@ import { Products } from '../pages/Products';
 import { Mission } from './Mission';
 import { Team } from './Team';
 import { Reviews } from './Reviews';
-import { SharedLayout } from './SharedLayout';
+import { Container, Header, Logo, Link } from './App.styled';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />}>
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
           <Route path="mission" element={<Mission />} />
           <Route path="team" element={<Team />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="products" element={<Products />} />
-        <Route path="products/:id" element={<ProductDetails />} />
-      </Route>
-    </Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </Container>
   );
 };
 
