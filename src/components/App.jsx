f30da1ed4d2da8f3531a868cbcd958b48d1d67c3
RomaSamkov <Romansamkov.rs@gmail.com>
2022-07-25 13:31:31 +0300

    add index routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,35 +6,22 @@ import { Products } from '../pages/Products';
 import { Mission } from './Mission';
 import { Team } from './Team';
 import { Reviews } from './Reviews';
-import { Container, Header, Logo, Link } from './App.styled';
+import { SharedLayout } from './SharedLayout';
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{' '}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
           <Route path="mission" element={<Mission />} />
           <Route path="team" element={<Team />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-      </Routes>
-    </Container>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
 
